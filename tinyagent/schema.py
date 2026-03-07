3062ba7c86edb63081de4dcde69e9de460d7877f
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-03-07 08:16:30 +0530

    Added a helper function to propmt class that will now allow user to give system prompts.

--- tinyagent/schema.py
+++ tinyagent/schema.py
@@ -3,6 +3,7 @@ from typing import Literal
 from pydantic import BaseModel, Field, Json
 from typing import Any
 
+
 class ToolCall(BaseModel):
     tool_id: str = Field(description='Id of the tool')
     tool_args: dict = Field(description='Mapping of tool arg name and tool arg value')
@@ -14,7 +15,7 @@ class SubAgentCall(BaseModel):
 
 
 class Content(BaseModel):
-    response: Any| None = Field(default=None, description='Response')
+    response: Any | None = Field(default=None, description='Response')
     tool_calls: list[ToolCall] = Field(default=[], description='list of tool calls')
     sub_agent_calls: list[SubAgentCall] = Field(default=[], description='list of sub agent calls')
     exit: bool = Field(default=False, description='Continue evaluation or not')
@@ -30,3 +31,6 @@ class Prompt(BaseModel):
     role: str
     content: str
     tool_call_id: str | None = None
+
+    def update(self, prompt: Prompt):
+        self.content += self.content + '\n' + prompt.content
